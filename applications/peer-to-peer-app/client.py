5004498cf477423c437ef486f9ef74662bc08804
Cory Lewis <cjl9703@yahoo.com>
2019-11-12 16:37:25 -0800

    (add) client.py implemented, verbose logging added as well

--- applications/peer-to-peer-app/client.py
+++ applications/peer-to-peer-app/client.py
@@ -6,48 +6,44 @@ assignment #1.
 """
 import socket
 import pickle
+from logging import Logging
 
 class Client(object):
 
     def __init__(self):
-        """
-        TODO: implement this contructor
-        Class contructor
-        """
-        self.client_socket = None # create here your client tcp socket
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.logging = Logging()
 
-    def connect(self, ip_adress, port):
-        """
-        TODO: Implement connection to server sockets
-        :param ip_adress:
-        :param port:
-        :return: VOID
-        """
-        pass
+    def connect(self, host_ip, port):
+        try:
+            self.client_socket.connect((host_ip, port))
+            self.logging.log("client.py -> connect", "connected to host: " + str(host_ip) + ":" + str(port))
+        except socket.error as err:
+            self.logging.log("client.py -> connect", "failed to connect to host on: " + str(host_ip) + ":" + str(port), 2, str(err))
 
     def send(self, data):
-        """
-        TODO: Implement client socket send method
-        :param data: raw_data. This data needs to be
-                     serialized inside this method
-                     with pickle before being sent.
-                     You can also serialize objects
-                     with pickle
-        :return: VOID
-        """
-        pass
+        try:
+            serialized = pickle.dumps(data)
+            self.client_socket.send(serialized)
+            self.logging.log("client.py -> send", "data sent" + str(data))
+        except socket.error as err:
+            self.logging.log("client.py -> send", "could not send to socket", 2, str(err))
 
-    def recieve(self, memory_allocation_size):
-        """
-        TODO: implement receives data from server socket
-        :param memory_allocation_size:
-        :return: deserialized data
-        """
-        return None
+    def receive(self, memory_allocation_size):
+        try:
+            serialized_data = self.server.recv(memory_allocation_size)
+            deserialized = pickle.loads(serialized_data)
+            self.logging.log("client.py -> receive", "received data: " + str(deserialized))
+            return deserialized
+        except socket.error as err:
+            print("socket recv failed with error %s" % err)
+            self.logging.log("client.py -> ", "receive", 2, str(err))
+        except pickle.UnpicklingError as err:
+            self.logging.log("client.py -> receive", "unpickling error!", 2, str(err))
+
+        self.logging.log("client.py -> receive", "an exception occured and receive is returning empty data!!!", 3)
+        return {}
 
     def close(self):
-        """
-        TODO: implement the close mechanish of a client socket
-        :return: VOID
-        """
-        pass
+        self.logging.log("client.py -> close", " socket closed!")
+        self.client_socket.close()
